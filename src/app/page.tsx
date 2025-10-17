b99b38f32bbebdda3df97010897cc986288b98ea
Daniyalk0 <getdaniyalkhan@gmail.com>
2025-10-17 15:15:35 +0530

    Implement the UI of seasonal highlights

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,10 @@ import React, { useEffect } from 'react';
 import Home from './(homePage)/Home';
 import { useDispatch } from 'react-redux';
 import { fetchProducts } from '../store/productsSlice';
+import { AppDispatch } from '../store/store';
 
 const Page = () => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
     dispatch(fetchProducts());
